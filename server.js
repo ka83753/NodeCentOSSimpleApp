106ee28f468fbc74107e2e03eb3acaf06e424f81
Keith Apps <github@apps.org.uk>
2018-06-28 09:55:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World from Distelli. Your Node.js application is working!\n');
-}).listen(8004);
+    response.end('Hello World from Distelli. Your Node.js application is working in test!\n');
+}).listen(8005);
 
 console.log('Server started');
